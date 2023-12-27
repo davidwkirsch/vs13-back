e78f3b78944ec051d63fbeaf869dc9738631c3a8
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-27 08:20:57 -0300

    exercicio 5 pronto

--- 01-java/Aula-01/src/Exercicio05.java
+++ 01-java/Aula-01/src/Exercicio05.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Exercicio05 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a base do retângulo: ");
+        int base = sc.nextInt();
+        System.out.print("Digite a altura do retângulo: ");
+        int altura = sc.nextInt();
+
+        int area = base * altura;
+        System.out.println("\nÁrea do retângulo: " + area);
     }
 }
