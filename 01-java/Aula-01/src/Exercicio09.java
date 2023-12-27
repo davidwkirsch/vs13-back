9f73e830e4f7cbf87ce60a2dacef00feff834520
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-27 09:03:50 -0300

    exercicio 9 pronto

--- 01-java/Aula-01/src/Exercicio09.java
+++ 01-java/Aula-01/src/Exercicio09.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class Exercicio09 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o horário de início do jogo, digitando inicialmente as horas e depois os minutos!");
+        System.out.print("Horas: ");
+        int horaInicio = sc.nextInt();
+        System.out.print("Minutos: ");
+        int minutoInicio = sc.nextInt();
+
+        System.out.println("Digite o horário de término do jogo, digitando inicialmente as horas e depois os minutos!");
+        System.out.print("Horas: ");
+        int horaTermino = sc.nextInt();
+        System.out.print("Minutos: ");
+        int minutoTermino = sc.nextInt();
+
+        if (horaTermino < horaInicio)
+        {
+            horaTermino += 24;
+        }
+
+        int totalHorasEmMinutos = (horaTermino - horaInicio) * 60;
+        int totalMinutos = minutoTermino - minutoInicio;
+        int tempoTotal = totalHorasEmMinutos + totalMinutos;
+
+        int duracaoHoras = tempoTotal / 60;
+        int duracaoMinutos = tempoTotal % 60;
+
+        if (duracaoHoras == 0 && duracaoMinutos == 00)
+        {
+            System.out.println("O jogo acabou assim que começou ou teve duração de 24 horas.");
+        }
+        else
+        {
+            System.out.println("O jogo durou " + duracaoHoras + "h" + duracaoMinutos + "min");
+        }
+
+
+
     }
 }
