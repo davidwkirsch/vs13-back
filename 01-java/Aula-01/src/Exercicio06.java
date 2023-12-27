2708c36bc155ef381330ba82cb01e62904fb59bd
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-27 08:28:13 -0300

    exercicio 6 pronto

--- 01-java/Aula-01/src/Exercicio06.java
+++ 01-java/Aula-01/src/Exercicio06.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Exercicio06 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o total de eleitores: ");
+        double totalEleitores = sc.nextDouble();
+
+        System.out.print("Digite o total de votos brancos: ");
+        double totalBrancos = sc.nextDouble();
+
+        System.out.print("Digite o total de nulos: ");
+        double totalNulos = sc.nextDouble();
+
+        System.out.print("Digite o total de válidos: ");
+        double totalValidos = sc.nextDouble();
+
+        double percentualBrancos = totalBrancos / totalEleitores * 100;
+        double percentualNulos = totalNulos / totalEleitores * 100;
+        double percentualValidos = totalValidos / totalEleitores * 100;
+
+        System.out.println("Total de eleitores: " + totalEleitores);
+        System.out.println("Percentual de votos brancos: " + percentualBrancos + "%");
+        System.out.println("Percentual de votos nulos: " + percentualNulos + "%");
+        System.out.println("Percentual de votos válidos: " + percentualValidos + "%");
     }
 }
