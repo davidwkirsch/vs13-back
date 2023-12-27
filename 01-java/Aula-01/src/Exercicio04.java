bdf097f90fcae51716207c0405c8be4f86646869
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-27 08:17:39 -0300

    exercicio 4 pronto

--- 01-java/Aula-01/src/Exercicio04.java
+++ 01-java/Aula-01/src/Exercicio04.java
@@ -2,6 +2,17 @@ public class Exercicio04 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int A = 10;
+        int B = 20;
+        int temp;
+
+        System.out.println("A antes da conversão: " + A);
+        System.out.println("B antes da conversão: " + B);
+
+        temp = B;
+        B = A;
+        A = temp;
+        System.out.println("A após a conversão: " + A);
+        System.out.println("B após a conversão: " + B);
     }
 }
