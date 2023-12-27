4f65fe4199debc4a0dcc4642690c9a21a33c5ae1
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-27 08:50:24 -0300

    exercicio 8 pronto

--- 01-java/Aula-01/src/Exercicio08.java
+++ 01-java/Aula-01/src/Exercicio08.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Exercicio08 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        double salarioNovo = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o salário atual: ");
+        double salarioAntigo = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Digite o cargo: ");
+        String cargo = sc.nextLine().toLowerCase();
+
+        salarioNovo = switch (cargo) {
+            case "gerente" -> salarioAntigo * 1.1;
+            case "engenheiro" -> salarioAntigo * 1.2;
+            case "tecnico" -> salarioAntigo * 1.3;
+            default -> salarioAntigo * 1.4;
+        };
+
+        System.out.println("Salário antigo: R$" + salarioAntigo);
+        System.out.println("Salário novo: R$" + salarioNovo);
+        System.out.println("Diferença: R$" + (salarioNovo - salarioAntigo));
     }
 }
