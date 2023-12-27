10a8e95c51ad7543407648d92228e50043791475
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-26 22:07:58 -0300

    Exercicio 3 pronto

--- 01-java/Aula-01/src/Exercicio03.java
+++ 01-java/Aula-01/src/Exercicio03.java
@@ -1,7 +1,83 @@
+import java.util.Scanner;
+
 public class Exercicio03 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma palavra para traduzir:");
+        String palavra = sc.nextLine().toLowerCase();
+
+        if(palavra.equals("cachorro"))
+        {
+            System.out.println("Cachorro em inglês é: Dog");
+        }
+        else if(palavra.equals("tempo"))
+        {
+            System.out.println("Tempo em inglês é: Time");
+        }
+        else if(palavra.equals("amor"))
+        {
+            System.out.println("Amor em inglês é: Love");
+        }
+        else if(palavra.equals("cidade"))
+        {
+            System.out.println("Cidade em inglês é: City");
+        }
+        else if(palavra.equals("feliz"))
+        {
+            System.out.println("Feliz em inglês é: Happy");
+        }
+        else if(palavra.equals("triste"))
+        {
+            System.out.println("Triste em inglês é: Sad");
+        }
+        else if(palavra.equals("poderia"))
+        {
+            System.out.println("Poderia em inglês é: Could");
+        }
+        else if(palavra.equals("deveria"))
+        {
+            System.out.println("Deveria em inglês é: Should");
+        }
+        else if(palavra.equals("dog"))
+        {
+            System.out.println("Dog em português é: Cachorro");
+        }
+        else if(palavra.equals("time"))
+        {
+            System.out.println("Time em português é: Tempo");
+        }
+        else if(palavra.equals("love"))
+        {
+            System.out.println("Love em português é: Amor");
+        }
+        else if(palavra.equals("city"))
+        {
+            System.out.println("City em português é: Cidade");
+        }
+        else if(palavra.equals("happy"))
+        {
+            System.out.println("Happy em português é: Feliz");
+        }
+        else if(palavra.equals("sad"))
+        {
+            System.out.println("Sad em português é: Triste");
+        }
+        else if(palavra.equals("should"))
+        {
+            System.out.println("Should em português é: Deveria");
+        }
+        else if(palavra.equals("could"))
+        {
+            System.out.println("Could em português é: Poderia");
+        }
+        else
+        {
+            System.out.println("Essa palavra não é válida.");
+        }
+
+
     }
 }
