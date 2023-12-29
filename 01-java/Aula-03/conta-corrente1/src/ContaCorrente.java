1a3e16fce8f2bab3b017d5967d23fb12df2303ff
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-28 21:53:49 -0300

    criadas alguns métodos

--- 01-java/Aula-03/conta-corrente1/src/ContaCorrente.java
+++ 01-java/Aula-03/conta-corrente1/src/ContaCorrente.java
@@ -5,6 +5,39 @@ public class ContaCorrente {
     double saldo,
     chequeEspecial;
 
-    
+    public void imprimirContaCorrente()
+    {
+        System.out.printf("Conta corrente: %s", this.numeroConta);
+    }
+
+    public boolean sacar(double valorSacado)
+    {
+        if (valorSacado > this.saldo)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valorSacado;
+            return true;
+        }
+
+    }
+
+    public boolean depositar(double valorDeposito)
+    {
+        this.saldo += valorDeposito;
+        return true;
+    }
+
+    public boolean retornarSaldoComChequeEspecial()
+    {
+        return true;
+    }
+
+    public boolean transferir(String ContaCorrente, double valor)
+    {
+        return true;
+    }
 
 }
