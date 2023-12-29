2a0494b1233ff090f4202e66130e887438273e31
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-28 21:25:27 -0300

    classe contato pronta

--- 01-java/Aula-03/conta-corrente1/src/Contato.java
+++ 01-java/Aula-03/conta-corrente1/src/Contato.java
@@ -1,2 +1,17 @@
 public class Contato {
+    String descricao,
+    telefone;
+    int tipo;
+
+    public void imprimirContato()
+    {
+        if (tipo == 1)
+        {
+            System.out.printf("Contato residencial: %s", telefone);
+        }
+        else
+        {
+            System.out.printf("Contato comercial: %s", telefone);
+        }
+    }
 }
