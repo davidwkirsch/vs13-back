da3f5ccda23f080f89768487316f62c5aa3afa6e
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2023-12-30 10:23:40 -0300

    add: saving's account functions

--- 01-java/Aula-03/conta-corrente1/src/ContaPoupanca.java
+++ 01-java/Aula-03/conta-corrente1/src/ContaPoupanca.java
@@ -1,3 +1,10 @@
-public class ContaPoupanca extends Conta {
+public class ContaPoupanca extends Conta implements Impressao{
+    private static double JUROS_MENSAL = 1.01;
+    public void creditarTaxa() {
+        super.setSaldo(super.getSaldo() * JUROS_MENSAL);
+    }
+    @Override
+    public void imprimir() {
 
+    }
 }
