5912affe8ea720d76390101d0b113dd781093d73
DavidKirsch-DBC <david.kirsch@dbccompany.com.br>
2024-01-17 08:23:22 -0300

    add: task-02

---
+++ 03-spring-web/Aula-02/task-02/PessoaAPI-Aula/src/main/java/br/com/dbc/vemser/pessoaapi/controller/ContatoController.java
@@ -0,0 +1,45 @@
+package br.com.dbc.vemser.pessoaapi.controller;
+
+import br.com.dbc.vemser.pessoaapi.entity.Contato;
+import br.com.dbc.vemser.pessoaapi.service.ContatoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contato") // localhost:8080/contato
+public class ContatoController {
+
+    private ContatoService contatoService;
+
+    public ContatoController() {
+        contatoService = new ContatoService();
+    }
+
+
+    @GetMapping // GET localhost:8080/contato
+    public List<Contato> list() {
+        return contatoService.list();
+    }
+
+    @GetMapping("/") // GET localhost:8081/contato/?pessoa=1
+    public List<Contato> listByName(@RequestParam("pessoa") Integer idPessoa) {
+        return contatoService.listByIdPessoa(idPessoa);
+    }
+
+    @PostMapping // POST localhost:8080/contato
+    public Contato create(@RequestBody Contato contato) {
+        return contatoService.create(contato);
+    }
+
+    @PutMapping("/{idContato}") // PUT localhost:8080/contato/1000
+    public Contato update(@PathVariable("idContato") Integer id,
+                         @RequestBody Contato contatoAtualizar) throws Exception {
+        return contatoService.update(id, contatoAtualizar);
+    }
+
+    @DeleteMapping("/{idContato}") // DELETE localhost:8080/contato/10
+    public void delete(@PathVariable("idContato") Integer id) throws Exception {
+        contatoService.delete(id);
+    }
+}
---
+++ 03-spring-web/Aula-02/task-02/PessoaAPI-Aula/src/main/java/br/com/dbc/vemser/pessoaapi/service/ContatoService.java
@@ -0,0 +1,57 @@
+package br.com.dbc.vemser.pessoaapi.service;
+
+import br.com.dbc.vemser.pessoaapi.entity.Contato;
+import br.com.dbc.vemser.pessoaapi.entity.Pessoa;
+import br.com.dbc.vemser.pessoaapi.repository.ContatoRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContatoService {
+
+    private ContatoRepository contatoRepository;
+
+    public ContatoService(){
+        contatoRepository = new ContatoRepository();
+    }
+
+    public Contato create(Contato contato){
+        return contatoRepository.create(contato);
+    }
+
+
+    public List<Contato> list(){
+        return contatoRepository.list();
+    }
+
+    public Contato update(Integer id,
+                         Contato contatoAtualizar) throws Exception {
+        Contato contatoRecuperado = getContato(id);
+
+        contatoRecuperado.setIdPessoa(contatoAtualizar.getIdPessoa());
+        contatoRecuperado.setNumero(contatoAtualizar.getNumero());
+        contatoRecuperado.setDescricao(contatoAtualizar.getDescricao());
+        contatoRecuperado.setTipoContato(contatoAtualizar.getTipoContato());
+
+        return contatoRecuperado;
+    }
+
+    public void delete(Integer id) throws Exception {
+        Contato contatoRecuperado = getContato(id);
+        contatoRepository.delete(contatoRecuperado);
+    }
+
+    public List<Contato> listByIdPessoa(Integer idPessoa) {
+        return contatoRepository.listByIdPessoa(idPessoa);
+    }
+
+    private Contato getContato(Integer id) throws Exception {
+
+        Contato contatoRecuperado = contatoRepository.list().stream()
+                .filter(contato -> contato.getIdContato().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Contato não encontrado!"));
+        return contatoRecuperado;
+    }
+
+}
