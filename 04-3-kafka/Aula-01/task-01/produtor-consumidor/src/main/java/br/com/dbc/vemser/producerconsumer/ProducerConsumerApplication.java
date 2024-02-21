cb81f8028d3ea3ff2d91bab4e5d39448c8c14a62
DavidKirsch-DBC <153624310+DavidKirsch-DBC@users.noreply.github.com>
2024-02-21 09:35:27 -0300

    task de kafka

---
+++ 04-3-kafka/Aula-01/task-01/produtor-consumidor/src/main/java/br/com/dbc/vemser/producerconsumer/ProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.producerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerConsumerApplication.class, args);
+	}
+
+}
---
+++ 04-3-kafka/Aula-01/task-01/produtor-consumidor/src/test/java/br/com/dbc/vemser/producerconsumer/ProducerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vemser.producerconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
